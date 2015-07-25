06f99af93bad282067bcd8fc3f915931672f73ea
sazonoff.dmitry2014 <sazonoff.dmitry2014@yandex.ru>
2015-07-25 17:17:57 +0300

    no message

--- core/src/org/abstractfactory/Unit.java
+++ core/src/org/abstractfactory/Unit.java
@@ -7,7 +7,7 @@ import org.epicgameheroesinterfaces.DrawableAction;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Unit extends BasicHero implements  DrawableAction {
+public class Unit extends BasicHero implements DrawableAction {
 
 	private Animation rest;
 	private Animation movement;
@@ -23,7 +23,7 @@ public class Unit extends BasicHero implements  DrawableAction {
 	Unit() {
 		setAction(Action.REST);
 	}
-	
+
 	public Boolean getFly() {
 		return fly;
 	}
@@ -31,68 +31,77 @@ public class Unit extends BasicHero implements  DrawableAction {
 	public void setFly(Boolean fly) {
 		this.fly = fly;
 	}
-	
+
 	@Override
 	public void setAction(Action action) {
-    	super.setAction(action);
-    	runTime = 0f;
-    }
-	
+		super.setAction(action);
+		runTime = 0f;
+	}
+
 	@Override
 	public void setRestAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		rest = anim;
 		rest.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 	}
 
 	@Override
 	public void setMovementAnimation(Animation anim) {
-		if (anim == null) return;
+		if (anim == null)
+			return;
 		movement = anim;
 		movement.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 	}
 
 	@Override
 	public void setDamageAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		damage = anim;
 		damage.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 	}
-	
+
 	@Override
 	public void setDieAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		die = anim;
 		die.setPlayMode(Animation.PlayMode.NORMAL);
 	}
+
 	@Override
 	public void setNearAttackAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		nearAttack = anim;
 		nearAttack.setPlayMode(Animation.PlayMode.NORMAL);
 	}
 
 	@Override
 	public void setFarAttackAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		farAttack = anim;
 		farAttack.setPlayMode(Animation.PlayMode.NORMAL);
 	}
 
 	@Override
 	public void setMagickAttackAnimation(Animation anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		magickAttack = anim;
 		magickAttack.setPlayMode(Animation.PlayMode.NORMAL);
-		
+
 	}
-	
+
 	@Override
 	public void setSpecialSkillsAnimation(Animation[] anim) {
-		if(anim == null) return;
+		if (anim == null)
+			return;
 		specialSkills = anim;
-		for(Animation a : anim) {
-			if(a == null) {
+		for (Animation a : anim) {
+			if (a == null) {
 				specialSkills = null;
 				return;
 			}
@@ -102,12 +111,19 @@ public class Unit extends BasicHero implements  DrawableAction {
 
 	@Override
 	public TextureRegion drawAction(Float runTime) {
-		this.runTime = runTime;
-		return selectAction().getKeyFrame(runTime);
+		this.runTime += runTime;
+		Animation selected = selectAction();
+		if (selected.getPlayMode() == Animation.PlayMode.NORMAL
+				&& selected.isAnimationFinished(this.runTime)) {
+			setAction(Action.REST);
+			this.runTime = 0f;
+		}
+		return selected == null ? rest.getKeyFrame(runTime) : selected
+				.getKeyFrame(runTime);
 	}
-	
+
 	private Animation selectAction() {
-		switch(getAction()) {
+		switch (getAction()) {
 		case REST:
 			return rest;
 		case MOVEMENT:
@@ -128,6 +144,5 @@ public class Unit extends BasicHero implements  DrawableAction {
 			return rest;
 		}
 	}
-
 
 }
